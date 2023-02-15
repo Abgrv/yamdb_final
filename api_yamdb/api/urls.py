5f4f3999b4d8190b84d5b9aee28492d770451c71
Abgrv <102848726+Abgrv@users.noreply.github.com>
2023-02-15 13:32:27 +0300

    views

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+
 from . import views
 
 router_v1 = routers.DefaultRouter()
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,7 +13,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
-
 from api.filters import TitleFilter
 from api.mixins import MixinViewSet
 from reviews.models import Category, Genre, Review, Title
@@ -125,8 +124,7 @@ class UsersViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         if request.method == 'PATCH':
             serializer = self.get_serializer(
-                request.user, data=request.data, partial=True
-            )
+                request.user, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             serializer.save(role=request.user.role)
             return Response(serializer.data, status=status.HTTP_200_OK)
