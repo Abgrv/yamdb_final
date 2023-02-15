2991382389f6ddb144b68871ffeaad235af039aa
Abgrv <102848726+Abgrv@users.noreply.github.com>
2023-02-15 13:56:08 +0300

    commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -121,13 +121,13 @@ class UsersViewSet(viewsets.ModelViewSet):
     def me(self, request):
         if request.method == 'GET':
             serializer = self.get_serializer(request.user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            # return Response(serializer.data, status=status.HTTP_200_OK)
         if request.method == 'PATCH':
             serializer = self.get_serializer(
                 request.user, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             serializer.save(role=request.user.role)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserRegistrationApiView(APIView):
